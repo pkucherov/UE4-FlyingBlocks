dc646156c3c2f12139b7216c926dac7e001c8caf
Pavel Kucherov <26567338+pkucherov@users.noreply.github.com>
2022-02-08 20:02:30 +0100

    Add create and delete buttons to HUD. Add Player base class

---
+++ Source/FlyingBlocks/Private/Player/BlockPlayer.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/BlockPlayer.h"
+
+// Sets default values
+ABlockPlayer::ABlockPlayer(): bSpawnBlock(true)
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABlockPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlockPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABlockPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/FlyingBlocks/Public/Player/BlockPlayer.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BlockPlayer.generated.h"
+
+UCLASS()
+class FLYINGBLOCKS_API ABlockPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABlockPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Flying Blocks")
+	void SetSpawnOnTouchState(bool bSpawn) { bSpawnBlock = bSpawn; }
+
+private:
+	bool bSpawnBlock;
+
+};
